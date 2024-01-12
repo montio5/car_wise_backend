f9c4bb23445aab22c55e06178d3f0fb77bf181c9
montio5 <moein@greenhabitat.at>
2024-01-12 23:05:46 +0330

    add to project

--- carwise/apps/reminder/models.py
+++ carwise/apps/reminder/models.py
@@ -1,1 +1,26 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
+
+
+# --------------------------------------
+class Car(models.Model):
+    unique_key = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+# --------------------------------------
+
+
+class CarCompany(models.Model):
+    car_company = models.CharField(max_length=256)
+
+
+# --------------------------------------
+
+
+class CarModel(models.Model):
+    unique_key = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    car_model = models.CharField(max_length=256)
+    car_company = models.ForeignKey(CarCompany, on_delete=models.CASCADE)
--- carwise/carwise/urls.py
+++ carwise/carwise/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import ObtainAuthToken
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/token/", ObtainAuthToken.as_view()),
-    path("", include("reminder.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("", include("apps.reminder.urls")),
 ]
