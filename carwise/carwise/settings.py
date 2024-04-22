26f4cc8797465dc8c51fc6b5f4246105836b3230
montio5 <moein@greenhabitat.at>
2024-04-22 20:49:13 +0330

    refactor: edit settings

--- carwise/carwise/settings.py
+++ carwise/carwise/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'whitenoise.runserver_nostatic',
     #
     "rest_framework",
     "rest_framework_simplejwt",
@@ -91,6 +92,7 @@ SWAGGER_SETTINGS = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -174,6 +176,7 @@ USE_THOUSAND_SEPARATOR = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
