d743be762a4e14e0d6110b72ddaa64f946bed643
montio5 <90924916+montio5@users.noreply.github.com>
2024-09-19 21:02:13 +0330

    refactor: edit settings address

--- carwise/apps/common/tasks.py
+++ carwise/apps/common/tasks.py
@@ -5,8 +5,10 @@ from django.contrib.auth.models import User
 from apps.common.notification import send_push_notification
 from apps.reminder.views.general_views import get_notification_for_user
 
-# Set up Django environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carwise.settings")
+# Set the environment variable to point to your Django settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carwise.carwise.settings")
+
+# Setup Django (required to access models and other Django features)
 django.setup()
 
 # Logging configuration (optional)
