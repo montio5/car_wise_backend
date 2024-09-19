7f4edf7b110206d51423bf34f26bdcf8dedf065f
montio5 <90924916+montio5@users.noreply.github.com>
2024-09-19 20:44:23 +0330

    update tasks

--- carwise/apps/common/tasks.py
+++ carwise/apps/common/tasks.py
@@ -1,14 +1,33 @@
+import os
+import django
+import logging
 from django.contrib.auth.models import User
-
 from apps.common.notification import send_push_notification
 from apps.reminder.views.general_views import get_notification_for_user
 
+# Set up Django environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carwise.settings")
+django.setup()
+
+# Logging configuration (optional)
+logging.basicConfig(level=logging.INFO)
+
 
 def send_notifications():
+    logging.info("Executing send_notifications function...")
     users = User.objects.all()
+
     for user in users:
-        send_push_notification(
-            user,
-            "daily check",
-            get_notification_for_user(user),
-        )
+        try:
+            # Send push notification to each user
+            message = get_notification_for_user(user)
+            send_push_notification(user, "daily check", message)
+            logging.info(f"Notification sent to {user.username}: {message}")
+        except Exception as e:
+            logging.error(f"Error sending notification to {user.username}: {e}")
+
+    logging.info("send_notifications function executed successfully!")
+
+
+if __name__ == "__main__":
+    send_notifications()
