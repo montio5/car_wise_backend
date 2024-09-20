2dc469b655856cf05643fdfa36007b35fa0ebd8e
montio5 <90924916+montio5@users.noreply.github.com>
2024-09-20 10:26:28 +0330

    create add files

---
+++ carwise/apps/common/management/commands/send_notifications.py
@@ -0,0 +1,24 @@
+import logging
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from apps.common.notification import send_push_notification
+from apps.reminder.views.general_views import get_notification_for_user
+
+
+class Command(BaseCommand):
+    help = "Send notifications to all users"
+
+    def handle(self, *args, **kwargs):
+        logging.info("Executing send_notifications function...")
+        users = User.objects.all()
+
+        for user in users:
+            try:
+                # Send push notification to each user
+                message = get_notification_for_user(user)
+                send_push_notification(user, "daily check", message)
+                logging.info(f"Notification sent to {user.username}: {message}")
+            except Exception as e:
+                logging.error(f"Error sending notification to {user.username}: {e}")
+
+        logging.info("send_notifications function executed successfully!")
