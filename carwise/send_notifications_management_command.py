1b3c8cb69a05344848e8f13d5dbafb0bee37471d
montio5 <90924916+montio5@users.noreply.github.com>
2024-09-20 11:54:50 +0330

    refactor: add translations

--- carwise/send_notifications_management_command.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from django.core.management.base import BaseCommand
-from apps.common.tasks import send_notifications
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        send_notifications()
